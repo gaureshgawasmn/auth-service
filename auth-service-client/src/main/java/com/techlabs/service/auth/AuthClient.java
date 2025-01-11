046fbe610db5cd80cf725ff903ab81330f7ba834
gaureshgawasmn <gaureshgawasmn@gmail.com>
2025-01-11 18:51:36 +0530

    added basic structure of working auth service

--- auth-service-client/src/main/java/com/techlabs/service/auth/AuthClient.java
+++ auth-service-client/src/main/java/com/techlabs/service/auth/AuthClient.java
@@ -1,2 +1,10 @@
-package com.techlabs.service.auth.client;public interface AuthClient {
+package com.techlabs.service.auth;
+
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface AuthClient {
+
+    @POST("/auth/authenticate")
+    boolean authenticate(@Body AuthRequest authRequest);
 }
