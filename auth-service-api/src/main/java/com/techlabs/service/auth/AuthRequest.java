567f762263b3be7c34f0aecbbbd9682620e68288
gaureshgawasmn <gaureshgawasmn@gmail.com>
2025-01-11 23:40:49 +0530

    added end points to add get users and authenticate user

--- auth-service-api/src/main/java/com/techlabs/service/auth/AuthRequest.java
+++ auth-service-api/src/main/java/com/techlabs/service/auth/AuthRequest.java
@@ -1,11 +1,14 @@
 package com.techlabs.service.auth;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class AuthRequest {
+    @NotNull
     private String username;
+    @NotNull
     private String password;
 }
